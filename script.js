114b5c7983ba6ee9fa8476141674dadc1057f481
AilesVax <raphael.vaxelaire10@gmail.com>
2025-09-25 09:52:20 +0200

    conversion + resultat afficher

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-fetch("https://url.https://v6.exchangerate-api.com/v6/7008e2ab61722b071e7bf8fe/endpoint")
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error("Error:", error));
+// Création d'une fonction qui affichera les messages d'erreurs
+const displayError = (message) => {
+  const error = document.createElement('p')
+  error.classList.add('red')
+  error.textContent = message
+  document.body.appendChild(error)
+}
 
-  
+const getData = async () => {
+  let devise1 = document.getElementById('devise1').value;
+  let devise2 = document.getElementById('devise2').value;
+  const response = await fetch(`https://v6.exchangerate-api.com/v6/7008e2ab61722b071e7bf8fe/pair/${devise1}/${devise2}`)
+    .catch(error => {
+      console.error("Error:", error)
+      displayError("Une erreur est survenue")
+    });
+
+    console.log(response)
+  if (response.status < 300) {
+    const data = await response.json();
+
+    const container = document.querySelector(".resultat");
+    let test;
+    let conversion;
+    let nombre = document.getElementById('nombre');
+    conversion = data.conversion_rate;
+    nombre.addEventListener('input', () => {
+     
+      test = Number(nombre.value) * conversion;
+      container.innerHTML = test
+    });
+
+console.log(conversion)
+    
+
+  }
+  else {
+    // Status code >= 300
+    displayError("Une erreur est survenue")
+  }
+}
+
+// On lance au chargement de la page l'appel API
+getData()
